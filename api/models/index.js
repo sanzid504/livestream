1deb49f6dafc840923b221df7d38763f12abfda0
Sanzid <mithylamine@gmail.com>
2025-01-30 16:59:43 +0600

    x

--- api/models/index.js
+++ api/models/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  "mysql://sheikh_hasina:^xbnjY-se6qmjCgFjRxnIyT>^fO<{61Y@ls-ff4ea189a95c93facdfc8b09c3a557cbad965897.c2oald75xobm.ap-south-1.rds.amazonaws.com/dbmaster",
+  "DBURL",
   { logging: false }
 );
 
